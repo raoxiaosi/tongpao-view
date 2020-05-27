1cb838d11e64d37f96869c008a7c48882567c73a
raojing <1770561005@qq.com>
2020-05-27 19:44:23 +0800

    update

---
+++ cpfile.sh
@@ -0,0 +1,14 @@
+
+rm -rf ./dist/bootstrap
+rm -rf ./dist/layui
+rm -rf ./dist/css
+rm -rf ./dist/js
+rm -rf ./dist/images
+
+cp -r ./src/bootstrap/ ./dist
+cp -r ./src/layui/ ./dist
+cp -r ./src/css/ ./dist
+cp -r ./src/js/ ./dist
+cp -r ./src/images/ ./dist
+
+
